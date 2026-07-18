5da0c358edbd5b2704c1bb2c2b0fc38aab20d69b
busecllskn <buseclskn738@gmail.com>
2026-07-19 01:25:03 +0300

    feat: add change timeout

--- KISISELWEBSITESI/src/api/axios.jsx
+++ KISISELWEBSITESI/src/api/axios.jsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://reqres.in/api', 
-  timeout: 5000,
+  timeout: 8000,
   headers: {
     'Content-Type': 'application/json',
-  },
+  }, 
 });
 
 export default api;
