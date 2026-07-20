6106411655fd039e9fac7a6c168a25967023d7dd
busecllskn <buseclskn738@gmail.com>
2026-07-21 00:54:23 +0300

    feat: change apply

--- KISISELWEBSITESI/src/App.jsx
+++ KISISELWEBSITESI/src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";  
+import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="bg-white min-h-screen text-black">
-      <Switch>  
+      <Switch>
         <Route exact path="/">
-          <Redirect to="/home" /> 
+          <Redirect to="/home" />
         </Route>
         <Route exact path="/home">
           <Home />
         </Route>
       </Switch>
-      <ToastContainer 
-        position="top-right"
+      <ToastContainer
+        position="bottom-right"
         autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -25,7 +25,6 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="colored"
       />
     </div>
   );
